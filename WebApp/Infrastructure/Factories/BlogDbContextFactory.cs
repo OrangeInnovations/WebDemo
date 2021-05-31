bd40d6ddac3985ffdf8bb85b42e2b54f098dbe72
David Chen <forward_chen@hotmail.com>
2021-05-31 15:50:06 -0400

    t

---
+++ WebApp/Infrastructure/Factories/BlogDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Demo.Domain.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Infrastructure.Factories
+{
+    public class BlogDbContextFactory : IDesignTimeDbContextFactory<BlogDbContext>
+    {
+        public BlogDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+               .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+               .AddJsonFile("appsettings.json")
+               .AddEnvironmentVariables()
+               .Build();
+
+            DbContextOptionsBuilder<BlogDbContext> optionsBuilder = new DbContextOptionsBuilder<BlogDbContext>();
+
+            optionsBuilder.UseSqlServer(config["ConnectionString"], sqlServerOptionsAction: o => o.MigrationsAssembly("WebApp"));
+
+            return new BlogDbContext(optionsBuilder.Options);
+        }
+    }
+}
